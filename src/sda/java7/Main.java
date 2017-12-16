ed1b7c7f67c7c81d4ab6729e765e190a3cbf602f
Iza <izabel.kulak@gmail.com>
2017-12-16 14:22:04 +0100

    komit b

--- src/sda/java7/Main.java
+++ src/sda/java7/Main.java
@@ -22,6 +22,7 @@ public class Main {
 
         System.out.println("Abcdeefefefedef");
         System.out.println("Abcdeefefefedef");
+        System.out.println("Abcdeefefefedef");
 
         System.out.println("branczebranczebrancze");
         System.out.println("cokolwiek3");
