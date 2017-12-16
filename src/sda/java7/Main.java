f77bca4ec6482f5d6000d790d51efd0f4942e2e7
Iza <izabel.kulak@gmail.com>
2017-12-16 14:22:18 +0100

    komit c

--- src/sda/java7/Main.java
+++ src/sda/java7/Main.java
@@ -31,6 +31,7 @@ public class Main {
 
 
         System.out.println("cos tam2ff");
+        System.out.println("cos tam2ff");
 
         System.out.println("Branch izy");
         System.out.println("Branch izy");
