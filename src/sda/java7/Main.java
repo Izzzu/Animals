2289a630e9925f98b42cde431fc7bfe693182956
Iza <izabel.kulak@gmail.com>
2017-12-16 13:10:39 +0100

    drugi komit na branchu

--- src/sda/java7/Main.java
+++ src/sda/java7/Main.java
@@ -7,10 +7,13 @@ public class Main {
 
     public static void main(String[] args) {
         SoundMaker cat = new Cat();
+
         SoundMaker dog = new Dog();
+
         SoundMaker girrafe = new Giraffe();
 
         List<SoundMaker> animals = new LinkedList<>();
+
         animals.add(cat);
         animals.add(dog);
         animals.add(girrafe);
